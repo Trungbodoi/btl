27fbc68692294c2d78ccd8ac38d41fc162fae154
Trungbodoi <95862456+Trungbodoi@users.noreply.github.com>
2024-10-31 10:46:32 +0700

    Use cookie instead

--- account/account.js
+++ account/account.js
@@ -1,4 +1,16 @@
-if (localStorage.getItem('isLoggedIn') === 'true') {
+// Hàm lấy giá trị của cookie theo tên
+function getCookie(name) {
+    const cookies = document.cookie.split("; ");
+    for (const cookie of cookies) {
+        const [key, value] = cookie.split("=");
+        if (key === name) {
+            return decodeURIComponent(value);
+        }
+    }
+    return null; // Trả về null nếu cookie không tồn tại
+}
+
+if (getCookie('isLoggedIn') === 'true') {
     const currentLocation = window.location.pathname; // Lấy đường dẫn hiện tại
     const isIndexPage = currentLocation.endsWith('index.html') || currentLocation.endsWith('/'); // Kiểm tra xem có phải là index.html hoặc thư mục gốc
 
@@ -8,4 +20,3 @@ if (localStorage.getItem('isLoggedIn') === 'true') {
         document.getElementById('customer').href = '../account/profile.html'; // Sử dụng ../
     }
 }
-
